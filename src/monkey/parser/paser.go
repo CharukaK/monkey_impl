826fcab0d55faaaa0352678cd5d050ce0cfb047f
CharukaK <charukakarunanayake@gmail.com>
2024-02-23 08:10:45 +0530

    --wip-- [skip ci]

--- src/monkey/parser/paser.go
+++ src/monkey/parser/paser.go
@@ -18,7 +18,39 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	prg := &ast.Program{}
+	prg.Statements = make([]ast.Statement, 0)
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			prg.Statements = append(prg.Statements, *stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return prg
+}
+
+func (p *Parser) parseStatement() *ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	return stmt
+}
+
+func (p *Parser) expectPeekType(tokenType token.TokenType) bool {
+	return p.peekToken.Type == tokenType
 }
 
 func New(l *lexer.Lexer) *Parser {
